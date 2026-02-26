6d599600dec8879969d6c76f76fdc04481770e7e
harshitarai07-wq <harshirai0315@gmail.com>
2026-02-27 00:51:47 +0530

    Add files via upload

---
+++ js introduction.js
@@ -0,0 +1,11 @@
+// java script is a cross platform , object based scripting language for client and serever application .
+// loosely typed language 
+//interpreted at just in time completion(runtime) rather than compile time
+//high level programming language and multi paradigm programming language
+// can be embedded in html and is used to make web pages interactive and dynamic
+// FEATURES- extends functionality of web pages 
+//control page elements
+// object oriented
+//speed and simplicity
+// js and java are different languages and have different syntax and semantics
+
